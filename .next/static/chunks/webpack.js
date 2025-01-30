525ef5c26ad4338b0bd7ea44238a1c99eb831d65
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-01-30 18:31:31 +0600

    refactor: add new webpack hot-update files and update app paths in manifest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aea54ffbfa323eb5"; }
+/******/ 		__webpack_require__.h = function() { return "2f914138ef7b9101"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
